94d3bfd9229fc40d21dd5fdb60bc93a66dc63fb7
Erik Kundt <bitshift@posteo.org>
2019-01-05 00:33:29 +0100

    Moves batch of end-to-end tests to isoltest.

--- test/libsolidity/semanticTests/exp_operator.sol
+++
@@ -1,9 +0,0 @@
-contract test {
-    function f(uint a) public returns(uint d) { return 2 ** a; }
-    function g(uint a, uint b) public returns(uint c) { return a ** b; }
-}
-// ----
-// f(uint256): 1
-// -> 2
-// g(uint256, uint256): 1, 1
-// REVERT # Argument encoding is not properly implemented, yet.
